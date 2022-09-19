e5780bb9ca22fb0965930c9a1c6da074e3d96064
gmbreguet <guy-michel.breguet@heig-vd.ch>
2022-09-19 17:37:13 +0200

    2022-09-19 17:37:13 - GMB

---
+++ 01_HelloWorld.cpp
@@ -0,0 +1,19 @@
+//---------------------------------------------------------
+// Demo           : 01_HelloWorld
+// Fichier        : 01_HelloWorld.cpp
+// Auteur(s)      : BREGUET Guy-Michel
+// Date           : 2021-10-07
+// But            : premier programme en C++
+// Modifications  :
+// Remarque(s)    : NILL
+//---------------------------------------------------------
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main () {
+   cout << "Hello World" << endl;
+   return 0;
+}
