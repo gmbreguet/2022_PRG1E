f1e56cb4d8c956a1a6e56d46d751db3c2492db1e
gmbreguet <guy-michel.breguet@heig-vd.ch>
2022-09-22 11:43:56 +0200

    2022-09-22 11:43:56 - GMB

---
+++ 2022-09-22_base.cpp
@@ -0,0 +1,34 @@
+//---------------------------------------------------------
+// Demo           : 2022-09-22_base
+// Fichier        : 2022-09-22_base.cpp
+// Auteur(s)      : BREGUET Guy-Michel
+// Date           : 2021-10-07
+// But            : Demo en classe
+// Modifications  :
+// Remarque(s)    : NILL
+//---------------------------------------------------------
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+   const int MAX1  = 12;
+   int const MAX2  = 12;
+
+   int coordX      = 2,    // initialisation
+       coordY      = 3;
+
+   int bleu      = 2;
+   int monEntier = 4.9;
+
+   bleu = 5;            // affectation
+   bleu = coordX + coordY;
+   bleu = MAX + 2 * bleu;
+
+   cout << "mon entier : " << monEntier << endl;
+
+
+   return 0;
+}
