e48178b38ec6abca226c7b11a86806eca4aa5095
gmbreguet <guy-michel.breguet@heig-vd.ch>
2022-12-08 16:28:38 +0100

    2022-12-08 16:28:38 - GMB

---
+++ 2022-12-08_ex_chap_7_pays.cpp
@@ -0,0 +1,35 @@
+//---------------------------------------------------------
+// Demo           : 2022-12-08_ex_chap_7_pays
+// Fichier        : 2022-12-08_ex_chap_7_pays.cpp
+// Auteur(s)      : BREGUET Guy-Michel
+// But            : exercices chap 7.3
+// Modifications  :
+// Remarque(s)    : NILL
+//---------------------------------------------------------
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+//--------------------------------------------------
+class Pays {
+
+private:
+
+};
+
+
+
+//--------------------------------------------------
+int main () {
+
+//   Pays Suisse("Suisse",  8'698'000,  41'285);
+//   Pays France("France", 67'500'000, 643'801);
+//   Pays Italie("Italie", 59'070'000, 301'230);
+
+   cout << endl;
+   return EXIT_SUCCESS;
+}
---
+++ 2022-12-08_ex_chap_7_point.cpp
@@ -0,0 +1,30 @@
+//---------------------------------------------------------
+// Demo           : 2022-12-08_ex_chap_7
+// Fichier        : 2022-12-08_ex_chap_7.cpp
+// Auteur(s)      : BREGUET Guy-Michel
+// But            : exercices chap 7.1 et 7.2
+// Modifications  :
+// Remarque(s)    : NILL
+//---------------------------------------------------------
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+//--------------------------------------------------
+class Point {
+public:
+
+private:
+};
+
+
+
+//--------------------------------------------------
+int main () {
+
+
+   cout << endl;
+   return EXIT_SUCCESS;
+}
